58e1853e43499afe4e9bdbc8b916da81ec08adcb
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2020-04-01 02:09:49 -0300

    Add checkbox assets

--- Crystal.Engine/UI/Themes/KenneyTheme.cs
+++ Crystal.Engine/UI/Themes/KenneyTheme.cs
@@ -12,20 +12,23 @@ namespace Crystal.Engine.UI.Themes
         private Dictionary<string, NinePatchImage> panelBackgrounds;
         private Dictionary<string, ButtonTheme> buttonThemes;
         private Dictionary<string, SliderTheme> sliderThemes;
+        private Dictionary<string, CheckboxTheme> checkboxThemes;
 
         public IFont SmallFont => fonts["Kenvector thin 12"];
         public IFont MediumFont => fonts["Kenvector thin 36"];
         public IFont BigFont => fonts["Kenvector thin 72"];
 
         public NinePatchImage PanelBackground => panelBackgrounds["blue"];
-
         public ButtonTheme ButtonTheme => ButtonThemes["blue"];
         public SliderTheme SliderTheme => sliderThemes["blue"];
+        public CheckboxTheme CheckboxTheme => checkboxThemes["blue"];
 
         public Dictionary<string, IFont> Fonts => fonts;
         public Dictionary<string, NinePatchImage> PanelBackgrounds => panelBackgrounds;
         public Dictionary<string, ButtonTheme> ButtonThemes => buttonThemes;
         public Dictionary<string, SliderTheme> SliderThemes => sliderThemes;
+        public Dictionary<string, CheckboxTheme> CheckboxThemes => checkboxThemes;
+
 
         public void Load(IContentManager cm)
         {
@@ -51,7 +54,8 @@ namespace Crystal.Engine.UI.Themes
                     (7, 7),
                     (92, 7),
                     (7, 92),
-                    (92, 92)
+                    (92, 92),
+                    25
                 )
             );
 
@@ -78,6 +82,15 @@ namespace Crystal.Engine.UI.Themes
                 new SliderTheme(
                     cm.Load<IDrawable>("engine://Themes/kenney/Blue/blue_slider_down"),
                     cm.Load<IDrawable>("engine://Themes/kenney/Grey/slider")
+                )
+            );
+
+            this.checkboxThemes = new Dictionary<string, CheckboxTheme>();
+            this.checkboxThemes.Add(
+                "blue",
+                new CheckboxTheme(
+                    cm.Load<IDrawable>("engine://Themes/kenney/Blue/blue_checkmark"),
+                    cm.Load<IDrawable>("engine://Themes/kenney/Grey/grey_box")
                 )
             );
         }
